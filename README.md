61c1da66de3e469435cdf92139a2772f920d7a82
Aditya Wagh <86471464+Adityaw12@users.noreply.github.com>
2022-01-16 10:24:20 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Weather App With React JS
+
+This project designed with HTML, CSS, BOOTSTRAP, JS, REACT JS .\
+I used **somteacodes/weatherapp**'s design for the UI design and made some minor changes to it.
+
+![image](https://user-images.githubusercontent.com/40702669/145445286-584e0c26-6c8a-4c87-9ec7-8fa67f542465.png)
+![image](https://user-images.githubusercontent.com/40702669/145445362-bad5199d-8382-4737-9567-cf4226977793.png)
+![image](https://user-images.githubusercontent.com/40702669/145445461-a5223e02-3eeb-4fcb-ac83-0fe755e280b0.png)
+
+
+
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import '../Style.css'
+import CitySearch from './CitySearch';
+import WeatherCard from './WeatherCard'
+
+class App extends React.Component{
+
+    state = {weatherResult: null}
+
+    onSearchSubmit = async (searchInputValue) => {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=ca261c971d5638db9d4d6cbccc1f093d`)
+        this.setState({weatherResult: response.data})
+     }
+
+    render() {
+        return(
+            <div className="container my-5">
+                
+                <h1 className="text-center title">Weather App</h1>
+                <br />
+                 <CitySearch onSearchSubmit = {this.onSearchSubmit} />
+                 {this.state.weatherResult ?  <WeatherCard weatherResult = {this.state.weatherResult} /> : <div></div>}
+                 
+                 <div className="copyright text-center">
+                     © Aditya Wagh 2022
+                 </div>
+            </div>
+        )
+    }
+
+}
+
+export default App;
